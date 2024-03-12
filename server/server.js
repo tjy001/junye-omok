843359e09962901f6066080a4eaa442d02097e51
Jun Ye Tan <jyjytan94@gmail.com>
2024-03-12 20:04:23 +0800

    default port to 3000

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const path = require('path');
 
-const port = process.env.PORT;
+const port = process.env.PORT === undefined ? 3000 : process.env.PORT;
 const router = require("./router");
 
 const app = express();
